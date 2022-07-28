08b651a8207d4c4e3a2d2b5120ca42f7ba1a1746
Recep Cafer Yavuz <cafer.yavuz@etiya.com>
2022-07-28 11:08:45 +0300

    ejs added

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('INDEX SAYFASI');
+
+
+// Template Engine
+app.set("view engine","ejs")
+
+app.use(express.static("public"))
+
+
+
+
+// Routes
+app.get(['/','/index'], (req, res) => {
+  res.status(200).render('index',
+  {page_name : "index"}
+  )
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about',
+  {page_name : "about"}
+  )
 });
 
 const PORT = 3000;
