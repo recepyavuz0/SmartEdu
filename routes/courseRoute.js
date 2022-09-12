5f67562338c09cacbc0f3e343b989f68bc411cb6
caferyavuz <caferyavuz134@gmail.com>
2022-09-12 18:08:04 +0300

    edit

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -8,5 +8,4 @@ const router = express.Router()
 router.route('/').post(courseController.createCourse); // hhtp://localhost:3000/courses kurs oluşturma
 router.route('/').get(courseController.getAllCourses); // hhtp://localhost:3000/courses kursları listeleme
 
-
 module.exports = router
